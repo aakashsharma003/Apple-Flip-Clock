fc31d357ce4f91e51959a7552bd14d12331b567a
Akash Sharma <aakash6263264@gmail.com>
2024-06-01 20:04:46 +0530

    adding responsiveness

--- index.js
+++ index.js
@@ -3,34 +3,18 @@ const sec = document.getElementsByClassName("seconds");
 const mins = document.getElementsByClassName("minutes");
 const zon = document.getElementsByClassName("time-zone");
 const clock = setInterval(getTime, 1000);
+
 function getTime() {
   const date = new Date();
-  const zone = date.toLocaleTimeString().slice(8);
-  const hours = date.toLocaleTimeString().slice(0, 1);
+  const zone = date.toLocaleTimeString().slice(-2);
+  const hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
+
   zon[0].innerHTML = `${zone}`;
-  if (hours < 10) {
-    hr[0].innerHTML = `0${hours}`;
-    hr[1].innerHTML = `0${hours}`;
-  } else {
-    hr[0].innerHTML = hours;
-    hr[1].innerHTML = hours;
-  }
 
-  if (minutes < 10) {
-    mins[0].innerHTML = `0${minutes}`;
-    mins[1].innerHTML = `0${minutes}`;
-  } else {
-    mins[0].innerHTML = minutes;
-    mins[1].innerHTML = minutes;
-  }
-
-  if (seconds < 10) {
-    sec[0].innerHTML = `0${seconds}`;
-    sec[1].innerHTML = `0${seconds}`;
-  } else {
-    sec[0].innerHTML = seconds;
-    sec[1].innerHTML = seconds;
-  }
+  hr[0].innerHTML = hr[1].innerHTML = hours < 10 ? `0${hours}` : hours;
+  mins[0].innerHTML = mins[1].innerHTML =
+    minutes < 10 ? `0${minutes}` : minutes;
+  sec[0].innerHTML = sec[1].innerHTML = seconds < 10 ? `0${seconds}` : seconds;
 }
