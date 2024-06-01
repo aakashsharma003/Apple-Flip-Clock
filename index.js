a3a18301a5848c9266baf4827bed74b3c7fab114
Akash Sharma <aakash6263264@gmail.com>
2024-06-01 21:28:18 +0530

    adding seconds sound

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const hr = document.getElementsByClassName("hours");
 const sec = document.getElementsByClassName("seconds");
 const mins = document.getElementsByClassName("minutes");
 const zon = document.getElementsByClassName("time-zone");
+const tickSound = document.getElementById("tick-sound");
+
 const clock = setInterval(getTime, 1000);
 
 function getTime() {
   const date = new Date();
-  const zone = date.toLocaleTimeString().slice(-2);
+  const zone = date.toLocaleTimeString().slice(-2); // AM or PM
   const hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
@@ -17,4 +19,7 @@ function getTime() {
   mins[0].innerHTML = mins[1].innerHTML =
     minutes < 10 ? `0${minutes}` : minutes;
   sec[0].innerHTML = sec[1].innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+
+  tickSound.currentTime = 0; // Reset the audio to the beginning
+  tickSound.play(); // Play the audio
 }
